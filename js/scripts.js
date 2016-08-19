aab2626434d22a193fcc414e2184c19dde1bd581
Sheena Nickerson <sheenanick@gmail.com>
2016-08-19 09:09:02 -0700

    add js function to replace multiples of 3 with ping (Spec #2), and add output div to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
 
 var inputNumber;
 var list = [];
+var pingPongArray = [];
 
 function numberList(number) {
-  alert("grr");
-  for (i=1; i<=number; i++) {
+  for (var i=1; i<=number; i++) {
     var result = number-(number-i)
     list.push(result);
   }
   return list;
 }
 
-// function pingPong(number) {
-//
-// }
+function pingPong(array) {
+  for (var i=0; i<array.length; i++)
+    if(array[i] % 3 === 0){
+      pingPongArray.push("ping");
+    } else {
+      pingPongArray.push(array[i]);
+    }
+}
 
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     inputNumber = parseInt($("#input").val());
-    numberList(inputNumber);
-    // $("#output").text(output);
+    pingPong(numberList(inputNumber));
+    $("#output").text(pingPongArray);
   });
 });
